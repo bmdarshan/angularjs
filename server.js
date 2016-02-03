203ae5c767e82df6d12de355d4fe96a3b009d8a0
bmdarshan <darshan.bmg@gmail.com>
2016-02-03 17:24:19 -0500

    remove db credentials

--- server.js
+++ server.js
@@ -2,21 +2,20 @@
 
 // set up ========================
 var express  = require('express');
-var app      = express();                               // create our app w/ express
-var mongoose = require('mongoose');                     // mongoose for mongodb
-var morgan = require('morgan');             // log requests to the console (express4)
-var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
-var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
+var app      = express();                               
+var mongoose = require('mongoose');                     
+var morgan = require('morgan');             
+var bodyParser = require('body-parser');    
+var methodOverride = require('method-override'); 
 
 // configuration =================
 
-mongoose.connect('mongodb://bmdarshan:369hmbdarshanppg@ds043062.mongolab.com:43062/angularjsbmdarshan');     // connect to mongoDB database on mongolab cloud
 
-app.use(express.static(__dirname + '/app'));                    // set the static files location /public/img will be /img for users
-app.use(morgan('dev'));                                         // log every request to the console
-app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
-app.use(bodyParser.json());                                     // parse application/json
-app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+app.use(express.static(__dirname + '/app'));                    
+app.use(morgan('dev'));                                         
+app.use(bodyParser.urlencoded({'extended':'true'}));            
+app.use(bodyParser.json());                                    
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 app.use(methodOverride());
 
 var db = mongoose.connection;
@@ -25,7 +24,6 @@ db.once('open', function() {
   console.log('db connection succesful');
 });
 
-// listen (start app with node server.js) ======================================
 app.listen(9000);
 console.log("App listening on port 9000");
 
@@ -39,7 +37,7 @@ var ExpenseSchema = new mongoose.Schema({
 var Expense = mongoose.model('Expense', ExpenseSchema);
 
 app.get('/', function(req, res) {
-  res.sendfile('app/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+  res.sendfile('app/index.html');
 });
 
 app.get('/api/expenses', function (req, res, next) {
