e020768d916954c76535d3e654826915402e848e
Darshan Mukunda <darshan.bmg@gmail.com>
2016-02-05 16:28:41 -0500

    Update app.js

--- app/app.js
+++ app/app.js
@@ -1,8 +1,6 @@
-// declare a module
+
 var myAppModule = angular.module('accountingApp', ['ngRoute']);
 
-// configure the module.
-// in this example we will create a greeting filter
 myAppModule.config(function($routeProvider) {
   $routeProvider
    .when('/', {
